427735b4359e03090abfa3ec8acf6bec4698b54c
Hakan <52114496+celikdev@users.noreply.github.com>
2023-10-13 20:32:02 +0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#0F0F0F",
+        secondary: "#1E1B1E",
+        tertiary: "#F0FFFF",
+        yellow: "#FFAC5A",
+        orange: "#FF754A",
+        shy: "#7d5fff",
+        green: "#00b894",
+        blue: "#0984e3",
       },
     },
   },
   plugins: [],
-}
+};
